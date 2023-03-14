18bfc40bd98a9cfac74ad1f010148942752fc5b8
LuisPati <126108089+LuisPati11@users.noreply.github.com>
2023-03-14 22:16:31 +0200

    Clase 1

---
+++ lecture_1.py
@@ -0,0 +1,45 @@
+'''
+CURSO PYTHON
+
+Python es un lenguaje de programación de alto nivel y propósito general
+- Interpretado
+- Tipado dinámicamente
+- Todo es un objeto
+- Multiplataforma
+- Para principiantes
+
+Python es actualmente (marzo de 2023) el lenguaje de programación
+más popular del mundo.
+
+PYPL index: #1 (http://pypl.github.io/PYPL.html)
+
+Comparación Hola mundo! en otros lenguajes de programación vs python
+
+------------------------------------------------------------------------
+JAVA
+
+public class hello{
+    public static void main (String [] args){
+        System.out.println("Hola mundo!")
+    }
+}
+-------------------------------------------------------------------------
+C
+#include "stdio.h"
+int main(){
+    printf("Hola mundo\n")
+}
+-------------------------------------------------------------------------
+
+'''
+#Python
+print("Hola mundo!")
+
+
+'''
+En está primera clase hemos visto: 
+- En que consiste python 
+- La diferencia con otros lenguajes
+- Como escribir comentarios -> las dos opciones son con # (comentarios de una línea) ó con tres comillas para abrir y tres para cerrar, cometarios de varias líneas
+- Y nuestras primeras líneas de código el famoso Hola Mundo!
+'''
