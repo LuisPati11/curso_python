83eecb7e54fc56969c2531c7b6c883434a07bb44
LuisPati <126108089+LuisPati11@users.noreply.github.com>
2023-03-21 21:33:13 +0200

    Clase 5 hasta 2.3

--- clase_5/10_clase_y_objetos.py
+++ clase_5/10_clase_y_objetos.py
@@ -19,8 +19,8 @@ class Clase:
         self.b = 'Draco Malfoy'
 
 obj = Clase()
-print(obj.a)
-print(obj.b)
+#print(obj.a)
+#print(obj.b)
 
 # Utilizar argumentos por defecto en lugar de inicializadores múltiples
 class Clase:
@@ -31,9 +31,9 @@ class Clase:
 obj1 = Clase()
 obj2 = Clase(a=777)
 obj3 = Clase(a=777, b='Hemione Granger')
-print(obj1.a, obj1.b)
-print(obj2.a, obj2.b)
-print(obj3.a, obj3.b)
+#print(obj1.a, obj1.b)
+#print(obj2.a, obj2.b)
+#print(obj3.a, obj3.b)
 
 # 10.3 Atributos
 # Los atributos de instancia se establecen normalmente en el inicializador:
@@ -43,6 +43,75 @@ class Clase:
 
 obj1 = Clase('Luis')
 obj2 = Clase('Adrián')
-print(obj1.nombre)
-print(obj2.nombre)
+#print(obj1.nombre)
+#print(obj2.nombre)
 
+# 10.2.1 Propiedades
+class PythonGetSet:
+    def __init__(self, nombre):
+        self._nombre = nombre
+    
+    @property
+    def nombre(self):
+        #print('Llamada al get')
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, value):
+        #print('Llamada al set')
+        self._nombre = value
+
+    @nombre.deleter
+    def nombre(self):
+        #print('Llamada a borrar')
+        del self._nombre
+
+obj = PythonGetSet('Luis')
+
+obj.nombre = 'Beatriz'
+#print(obj.nombre)
+del obj.nombre
+
+# 10.2 Funciones integradas
+'''
+• hasattr
+• getattr
+• setattr
+• delattr
+Útil cuando no se conoce el nombre del atributo en el momento de escribir el código
+'''
+
+# hasattr comprueba si el objeto tiene algún atributo
+class DemoAtributos:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+obj = DemoAtributos('Luispa')
+obj.numero = 0
+
+#print(hasattr(obj, 'nombre'))
+#print(hasattr(obj, 'numero'))
+#print(hasattr(obj, 'apellido'))
+
+# getattr devuelve el valor del atributo especificado. También puede devolver algún valor por defecto, en caso de que el atributo solicitado no exista.
+class DemoAtributo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+obj = DemoAtributo('Luis')
+
+#print(getattr(obj, 'nombre'))
+#print(getattr(obj, 'no_existe', 'No existe ningún atributo'))
+
+# setattr establece (o sobrescribe) el valor del atributo especificado
+
+class DemoAtributo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+obj = DemoAtributo('Luis')
+
+setattr(obj, 'nuevo_atributo', 'Patiño')
+setattr(obj, 'nombre', 'Adrián')
+print(obj.nuevo_atributo)
+print(obj.nombre)
